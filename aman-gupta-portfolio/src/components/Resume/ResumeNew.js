cd747ed7f251eb648adcc370dde903215d677fe4
amangupta3317 <amangupta3317@gmail.com>
2024-04-18 11:29:23 +0530

    new

--- aman-gupta-portfolio/src/components/Resume/ResumeNew.js
+++ aman-gupta-portfolio/src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/aman_gupta_resume_1 (6).pdf";
+import pdf from "../../Assets/../Assets/Aman_Gupta_Photo_resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
