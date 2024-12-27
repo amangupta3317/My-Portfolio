f735cff791e1ba3bad46bc3130d75a187936ec1e
amangupta3317 <113909929+amangupta3317@users.noreply.github.com>
2024-12-27 12:21:06 +0530

    Update Home2.js

--- aman-gupta-portfolio/src/components/Home/Home2.js
+++ aman-gupta-portfolio/src/components/Home/Home2.js
@@ -15,24 +15,22 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I am an insightful person who always believed in hardwork with
-              consistency. I am a dedicated person towards my goal. I fell in
-              love with programming and I have at least learnt something, I
-              think… 🤷‍♂️
+              I am tech enthusiastic person who loves to "code" and "decode".
+              Proficient in C, C++ and Data Structures & Algorithm(DSA) makes
+              me a strong candidate.
+              Working as a DSA Mentor at GeeksforGeeks, I am happy to help 
+              others in enhancing their coding skills. 
+              
+              
+             
+             
               <br />
-              <br />I am fluent in classics like
+              My field of Interest -  &nbsp;
               <i>
-                <b className="purple"> C, C++, Python. </b>
+                <b className="purple">Web Technologies </b> and
+                <b className="purple">DevOps</b> and <b className="purple">Cloud Computing</b>.
               </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to <b className="purple">DevOps.</b>
-              </i>
-              <br />I am able to quickly learn new technologies and am always
-              eager to take on new challenges. I am confident that I have the
+              <br />I am confident that I have the
               skills and the drive to succeed in the tech industry.
               {/* <br /> */}
               {/* Whenever possible, I also apply my passion for developing products
